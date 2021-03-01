4597170f5b38903731d60ab551adc24b4eb2ea71
Adrian <adrian.korzan@gmail.com>
2021-03-01 16:00:40 +0100

    Updates

---
+++ precache-manifest.81efcb06235c2f32241eb88640c958ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59fd889900f9fc6c76ebdd2ed428773",
+    "url": "/kursBitCoin/index.html"
+  },
+  {
+    "revision": "fd10fcf92cfdb422498c",
+    "url": "/kursBitCoin/static/css/main.44a4df72.chunk.css"
+  },
+  {
+    "revision": "07e30c276a451dc168c5",
+    "url": "/kursBitCoin/static/js/2.236262ab.chunk.js"
+  },
+  {
+    "revision": "fd10fcf92cfdb422498c",
+    "url": "/kursBitCoin/static/js/main.c4334920.chunk.js"
+  },
+  {
+    "revision": "5c55c396d8bbc26efa50",
+    "url": "/kursBitCoin/static/js/runtime~main.43e93c16.js"
+  }
+]);
